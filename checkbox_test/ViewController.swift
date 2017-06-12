2eb00c1722c1597249b091523220adf695a71cbc
TsungHan <nick.yu@hzn.com.tw>
2017-06-12 14:00:36 +0800

    Initial Commit

---
+++ checkbox_test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  checkbox_test
+//
+//  Created by TsungHan on 2017/6/12.
+//  Copyright © 2017年 TsungHan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
